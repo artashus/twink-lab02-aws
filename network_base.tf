138531725c2528761da00fa2230deaf30acc5697
Tigran Arabadjyan <arabadj@orgrimmar.local>
2020-04-28 08:39:47 +0400

    add nacl

--- network_base.tf
+++ network_base.tf
@@ -8,3 +8,32 @@ resource "aws_vpc" "lab02" {
 
 }
 
+resource "aws_network_acl" "lab02" {
+
+  vpc_id = aws_vpc.lab02.id
+  subnet_ids = [aws_subnet.lab02_web.id]
+
+  ingress {
+    protocol   = "all"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+  
+  egress {
+    protocol   = "all"
+    rule_no    = 200
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+
+  tags = {
+    Name = "lab02"
+  }
+
+}
+
